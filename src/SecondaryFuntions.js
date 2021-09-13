8fa448177e7ad10f4ce373183202a73c9732a9ad
nombre <daniel.b100726@gmail.com>
2021-09-12 21:53:12 -0400

    Formateo general del código y limpieza

--- src/SecondaryFuntions.js
+++ src/SecondaryFuntions.js
@@ -1,58 +1,59 @@
-export const SumArray = (digit) => {//función que toma un número y suma todas sus cifras
-    let arr = [];
-    let n = digit.toString();
-    arr = n.split("");
-    return arr.reduce((acum,val) => acum = acum + parseInt(val),0);
-}
+export const SumArray = (digit) => {
+  //función que toma un número y suma todas sus cifras
+  let arr = [];
+  let n = digit.toString();
+  arr = n.split("");
+  return arr.reduce((acum, val) => (acum = acum + parseInt(val)), 0);
+};
 
-export const ReduceArray = (acum,val) => {
-    return acum + parseInt(val)
-}
+export const ReduceArray = (acum, val) => {
+  return acum + parseInt(val);
+};
 
 export const LongCardNumber = (number) => {
-    const long = number.length
-    if ((long < 13) || (long > 19)) {
-        return false
-    } else {
-        return true
-    }
-}
+  const long = number.length;
+  if (long < 13 || long > 19) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 export const Maskify = (array) => {
-    const arr1 = array.slice("")//hacemos una copia de array en arr1, de esta manera array no será modificado
-    const arr2 = []
-    for (let i = 0; i < array.length -4; i++) {
-        arr2[i] = "#"
-        arr1[i] = arr2[i]
-    }
-    const maski = arr1.reduce((acum, val) => acum + val)
-    return maski
-}
+  const arr1 = array.slice(""); //hacemos una copia de array en arr1, de esta manera array no será modificado
+  const arr2 = [];
+  for (let i = 0; i < array.length - 4; i++) {
+    arr2[i] = "#";
+    arr1[i] = arr2[i];
+  }
+  const maski = arr1.reduce((acum, val) => acum + val);
+  return maski;
+};
 
 export const hideWindow = (win) => {
-    if (win.classList.contains("hidden")) {
-        win.classList.remove("hidden")
-    } else {
-        win.classList.add("hidden")
-    }
-}
+  if (win.classList.contains("hidden")) {
+    win.classList.remove("hidden");
+  } else {
+    win.classList.add("hidden");
+  }
+};
 
 export const errorMessage = () => {
-    swal({
-        title: "Tarjeta Inválida",
-        text: "Por favor revisa tus datos para continuar con la compra",
-        icon: "error",
-    })
-}
+  swal({
+    title: "Tarjeta Inválida",
+    text: "Por favor revisa tus datos para continuar con la compra",
+    icon: "error",
+  });
+};
 
 export const succesMessage = (arrayCard, userName, win) => {
-    swal({
-        title: `Su tarjeta ${Maskify(arrayCard)} es válida.`,
-        text: `Gracias ${userName}, Tu compra se ha realizado exitosamente, disfruta de los servicios que te ofrece Batatabit.`,
-        icon: "success",
-    }).then(() => {
-        setTimeout(() => {
-            hideWindow(win)
-        }, 500)
-    })
-}
+  swal({
+    title: `Su tarjeta ${Maskify(arrayCard)} es válida.`,
+    text: `Gracias ${userName}, Tu compra se ha realizado exitosamente, disfruta de los servicios que te ofrece Batatabit.`,
+    icon: "success",
+  }).then(() => {
+    setTimeout(() => {
+      hideWindow(win);
+    }, 500);
+  });
+};
--- src/Validator.js
+++ src/Validator.js
@@ -1,65 +1,66 @@
-import  { SumArray, ReduceArray, LongCardNumber, hideWindow, succesMessage, errorMessage } from "./SecondaryFuntions.js"
+import {
+  SumArray,
+  ReduceArray,
+  LongCardNumber,
+  hideWindow,
+  succesMessage,
+  errorMessage,
+} from "./SecondaryFuntions.js";
 
-const p1 = document.getElementById("p1")
-const p2 = document.getElementById("p2")
-const p3 = document.getElementById("p3")
-const win = document.getElementById("valid_form")
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
+const p3 = document.getElementById("p3");
+const win = document.getElementById("valid_form");
 
 p1.addEventListener("click", () => {
-    hideWindow(win)
-})
+  hideWindow(win);
+});
 p2.addEventListener("click", () => {
-    hideWindow(win)
-})
+  hideWindow(win);
+});
 p3.addEventListener("click", () => {
-    hideWindow(win)
-})
-const btn = document.getElementById("validate")
+  hideWindow(win);
+});
+const btn = document.getElementById("validate");
 btn.addEventListener("click", (e) => {
-    e.preventDefault()
-    const n = document.getElementById("number_card")
-    const n2 = n.value.toString()
-    const u = document.getElementById("name_user")
-    const u2 = u.value.toString()
-    CardValidator(n2, u2)
-})
+  e.preventDefault();
+  const n = document.getElementById("number_card");
+  const n2 = n.value.toString();
+  const u = document.getElementById("name_user");
+  const u2 = u.value.toString();
+  CardValidator(n2, u2);
+});
 
 const CardValidator = (digit, userName) => {
-    const arrayCard = digit.split("")
-    const long = LongCardNumber(digit)
-    if (!long) {
-        errorMessage()
+  const arrayCard = digit.split("");
+  const long = LongCardNumber(digit);
+  if (!long) {
+    errorMessage();
+  } else {
+    const ValidDigit = LunhAlgorithm(arrayCard);
+    if (ValidDigit % 10 === 0 && long) {
+      succesMessage(arrayCard, userName, win);
     } else {
-        const ValidDigit = LunhAlgorithm(arrayCard)
-        if ((ValidDigit % 10 === 0) && long) {
-            succesMessage(arrayCard, userName, win)
-        } else {
-            errorMessage()
-        }
+      errorMessage();
     }
-}
+  }
+};
 const LunhAlgorithm = (arrayCard) => {
-    let arrayAux = []
-    for(let i = 0; i < arrayCard.length; i++) {
-        const mod = i % 2
-        const mult = 2 * (arrayCard[i])
-        const sum = SumArray(mult)
+  let arrayAux = [];
+  for (let i = 0; i < arrayCard.length; i++) {
+    const mod = i % 2;
+    const mult = 2 * arrayCard[i];
+    const sum = SumArray(mult);
 
-        if (mod === 0 && mult >= 10) {
-            arrayAux [i] = sum.toString()
-        } else if (mod === 0 && mult < 10) {
-            arrayAux [i] = mult.toString()
-        } else {
-            arrayAux [i] = arrayCard [i]
-        }
+    if (mod === 0 && mult >= 10) {
+      arrayAux[i] = sum.toString();
+    } else if (mod === 0 && mult < 10) {
+      arrayAux[i] = mult.toString();
+    } else {
+      arrayAux[i] = arrayCard[i];
     }
-    const ValidDigit = arrayAux.reduce(ReduceArray, 0)
-    console.log(ValidDigit)
-    return ValidDigit
-}
-
-// const card_string ="4137894711755904";
-// const card_string2 = "4095571174987314";
-// const card_string3 = "5117444488188614";
-// const card_string4 = "6165397064636554";
-// const card_string5 = "4083952015273";
+  }
+  const ValidDigit = arrayAux.reduce(ReduceArray, 0);
+  console.log(ValidDigit);
+  return ValidDigit;
+};
